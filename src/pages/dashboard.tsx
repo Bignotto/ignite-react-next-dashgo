d8f5fcb3242c4fbe43a873b49b7bb3be7dccd401
Thiago Bignotto <bignotto@gmail.com>
2021-05-22 16:37:08 -0300

    configured chart

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,55 @@
-import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts";
 
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-const options = {};
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-05-17T00:00:00.000Z",
+      "2021-05-18T00:00:00.000Z",
+      "2021-05-19T00:00:00.000Z",
+      "2021-05-20T00:00:00.000Z",
+      "2021-05-21T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityTo: 0.3,
+      opacityFrom: 0.7,
+    },
+  },
+};
 
 const series = [{ name: "series1", data: [578, 678, 631, 598, 650] }];
 
