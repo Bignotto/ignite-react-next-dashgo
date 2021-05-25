169bfc176cd22f443597fbb0f9771a845f1f737b
Thiago Bignotto <bignotto@gmail.com>
2021-05-25 12:17:21 -0300

    sidebar drawer component

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "@chakra-ui/react";
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiFile2Line,
+  RiGitMergeLine,
+  RiInputMethodLine,
+  RiSettings2Line,
+} from "react-icons/ri";
+
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="GERAL">
+        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+      </NavSection>
+      <NavSection title="AUTOMAÇÃO">
+        <NavLink icon={RiInputMethodLine}>Formlários</NavLink>
+        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+      </NavSection>
+      <NavSection title="ADMINISTRAÇÃO">
+        <NavLink icon={RiSettings2Line}>Configurações</NavLink>
+        <NavLink icon={RiFile2Line}>Relatórios</NavLink>
+      </NavSection>
+    </Stack>
+  );
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,32 +1,40 @@
-import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
 import {
-  RiContactsLine,
-  RiDashboardLine,
-  RiFile2Line,
-  RiGitMergeLine,
-  RiInputMethodLine,
-  RiSettings2Line,
-} from "react-icons/ri";
-import { NavLink } from "./NavLink";
-import { NavSection } from "./NavSection";
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={true} placement="left" onClose={() => {}}>
+        <DrawerOverlay>
+          <DrawerContent bg="gray.800" p="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
   return (
     <Box as="aside" w="64" mr="8">
-      <Stack spacing="12" align="flex-start">
-        <NavSection title="GERAL">
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Usuários</NavLink>
-        </NavSection>
-        <NavSection title="AUTOMAÇÃO">
-          <NavLink icon={RiInputMethodLine}>Formlários</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-        </NavSection>
-        <NavSection title="ADMINISTRAÇÃO">
-          <NavLink icon={RiSettings2Line}>Configurações</NavLink>
-          <NavLink icon={RiFile2Line}>Relatórios</NavLink>
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   );
 }
