2a8b372ca8990f956ee627db69177c55d9a1c72b
Thiago Bignotto <bignotto@gmail.com>
2021-05-25 12:35:13 -0300

    created sidebar drawer context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
@@ -11,6 +13,8 @@ export function Header() {
     lg: true,
   });
 
+  const { onOpen } = useSidebarDrawer();
+
   return (
     <Flex
       as="header"
@@ -22,6 +26,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWide && (
+        <IconButton
+          aria-label="Abrir menu"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
 
       {isWide && <SearchBox />}
