c7c03b977a2bc32cb6127ef01f29a7cb4461d03a
Thiago Bignotto <bignotto@gmail.com>
2021-05-25 10:15:01 -0300

    create header component files

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -78,8 +79,34 @@ export default function UserList() {
                   </Button>
                 </Td>
               </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Bignotto</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      bignotto@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>24 Mai 2021</Td>
+                <Td flex="1">
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
