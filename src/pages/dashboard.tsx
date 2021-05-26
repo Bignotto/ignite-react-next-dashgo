4dfd976159245e20740035bf5c68519d2f895505
Thiago Bignotto <bignotto@gmail.com>
2021-05-25 21:07:08 -0300

    responsive dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -58,16 +58,19 @@ export default function Dashboard() {
     <Flex direction="column" h="100vh">
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        {/* here goes the side bar and the dashboard */}
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8}>
-            <Text>Inscritos da Semana</Text>
+          <Box px={["2", "4"]} bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da Semana
+            </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8}>
-            <Text>Taxa de Abertura</Text>
+          <Box px={["2", "4"]} bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de Abertura
+            </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
